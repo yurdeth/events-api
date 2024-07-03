a6635b9f6162aec3fd24a6d4038081d99ad67db1
yurdeth <angelvasquez2570@gmail.com>
2024-07-02 20:00:38 -0600

    Opciones de cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // index.js
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors"; // Añade esta línea
 import eventRoutes from "./routes/eventRoutes";
 import { createDB } from "./config/database";
 import Event from "./models/eventModel";
@@ -15,6 +16,17 @@ const init = async () => {
 };
 
 const app = express();
+
+// Usa CORS sin opciones específicas (permite todas las solicitudes)
+app.use(cors());
+
+// Configura CORS
+// const corsOptions = {
+//   origin: ['http://127.0.0.1:5500', 'http://localhost:5500'], // Añade aquí los orígenes permitidos
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions)); // Usa CORS con las opciones configuradas
+
 app.use(bodyParser.json());
 app.use("/api", eventRoutes);
 
